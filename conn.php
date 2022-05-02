903068f903c09d067f99f19e337be2750f41b033
neliken <83429979+neliken@users.noreply.github.com>
2022-05-02 20:21:48 +0300

    fff

--- conn.php
+++ conn.php
@@ -3,6 +3,6 @@
 $link = new mysqli('localhost', 'root', '', 'jobs_for_ua');
 
 if(!$link){
-  die("ERROR: Could not connect. " . mysqli_error($con));
+  die("ERROR: Could not connectt. " . mysqli_error($con));
 }
  ?>
