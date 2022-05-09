02a6a1ffaaf541de67250af7ed1526a57bd0355c
neliken <83429979+neliken@users.noreply.github.com>
2022-05-09 22:01:07 +0300

    search

--- index1.php
+++ index1.php
@@ -9,7 +9,20 @@ include "db_connection.php";
        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
        <meta name="description" content="jobs">
        <meta name="author" content="IXOBIT LTD">
+       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Live Data Search using Multiple Tag in PHP with Ajax</title>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput-typeahead.css" />
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js" crossorigin="anonymous"></script>
 
+<style>
+.bootstrap-tagsinput {
+ width: 100%;
+}
+</style>
 
         <title>Jobs portal</title>
 
@@ -27,54 +40,90 @@ include "db_connection.php";
       </div>
 
         <center>
-        <br><br>
+        <br>
         <h1>Jobs for Refugees</h1>
-        <br><br>
-        <form action="/" >
-            <input type="text" placeholder="Search for jobs .." name="search">
-            <button type="submit"><i class="fa fa-search"></i></button>
-        </form>
-        <div class="search-box">
-          <input type="text" autocomplete="off" placeholder="Search country..." />
-          <div class="result"></div>
-        </div>
-        <br><br>
-        </center>
-        <div class="">
-          <center>
-          <table>
-            <tr>
-                <th>Position</th>
-                <th>Job description</th>
-                <th>Location</th>
-                <th>Employer</th>
-                <th>Send CV</th>
-            </tr>
-          <?php
-        $sql="Select * from jobs";
-        $result = mysqli_query($db_connection, $sql);
-        if($result){
-        while ($row =mysqli_fetch_assoc($result)) {
-          $name=$row['name'];
-          $description=$row['description'];
-          $category=$row['category'];
-          $company =$row['company'];
+      <div class="container">
+         <div class="form-group">
+          <div class="row">
+           <div class="col-md-10">
+            <input type="text" id="tags" class="form-control" data-role="tagsinput">
+           </div>
+           <div class="col-md-2">
+            <button type="button" name="search" class="btn btn-primary" id="search">Search</button>
+           </div>
+          </div>
+         </div>
+        <br>
+ <div class="table-responsive">
+  <div align="right">
+   <p><b>Total Records - <span id="total_records"></span></b></p>
+  </div>
+  <table class="table table-bordered table-striped">
+   <thead>
+    <tr>
+     <th>Name</th>
+     <th>Description</th>
+     <th>Category</th>
+     <th>Company</th>
+     <th>Send CV</th>
+    </tr>
+   </thead>
+   <tbody>
+   </tbody>
+  </table>
+ </div>
+</div>
+<div style="clear:both"></div>
+<br />
 
-    echo '
-        <tr>
-            <td>'.$name.'</td>
-            <td>'.$description.'</td>
-            <td>'.$category.'</td>
-            <td>'.$company.'</td>
-            <td><button>Send CV</button></td>
-        </tr>
-    ';
-  }
-}
- ?>
- </table>
- </center>
-        </div>
+<br />
+<br />
+<br />
     </body>
-
 </html>
+
+<script>
+$(document).ready(function(){
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"fetch.php",
+   method:"POST",
+   data:{query:query},
+   dataType:"json",
+   success:function(data)
+   {
+    $('#total_records').text(data.length);
+    var html = '';
+    if(data.length > 0)
+    {
+     for(var count = 0; count < data.length; count++)
+     {
+      html += '<tr>';
+      html += '<td>'+data[count].name+'</td>';
+      html += '<td>'+data[count].description+'</td>';
+      html += '<td>'+data[count].category+'</td>';
+      html += '<td>'+data[count].company+'</td>';
+      html += '<td><button>Send CV</button></td>';
+
+     }
+    }
+    else
+    {
+     html = '<tr><td colspan="5">No Data Found</td></tr>';
+    }
+    $('tbody').html(html);
+   }
+  })
+ }
+
+ $('#search').click(function(){
+  var query = $('#tags').val();
+  load_data(query);
+ });
+
+});
+</script>
